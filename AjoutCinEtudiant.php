f6a3e634f4310508b703938801ee5f47858bf581
yassine guidara <guidara.yassine@gmail.com>
2020-07-29 12:43:14 +0200

    ajout cin

---
+++ AjoutCinEtudiant.php
@@ -0,0 +1,30 @@
+<?php
+include 'header.php';
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP / MySQL</title>
+        <meta charset='utf-8'>
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>
+        <?php
+            $servname = "localhost"; $dbname = "crud"; $user = "root"; $pass = "";
+            try{
+                $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);/*Un utilisateur ne pourra jamais changer la structure d'une table,*pas la peine donc d'utiliser de requête préparée*/
+                $sql = "
+                  ALTER TABLE etudiant
+                  ADD Cin int
+                ";
+                $dbco->exec($sql);
+                echo 'Colonne ajoutée';
+            }
+            catch(PDOException $e){
+                echo "Erreur : " . $e->getMessage();
+            }
+        ?>
+    </body>
+</html>
